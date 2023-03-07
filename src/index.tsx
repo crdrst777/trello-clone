6d1736331db73a83f00521d34bcb329ccd4b93f0
HYEJINLEE <crdrst777@gmail.com>
2023-03-07 19:40:22 +0900

    feat: 초기 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import reset from "styled-reset";
+import { darkTheme } from "./theme";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const GlobalStyle = createGlobalStyle`
+${reset}
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Source Sans Pro', serif;
+  background-color: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.textColor};
+}
+a {
+  text-decoration: none;
+  color: inherit;
+  &:link,&:visited{
+  color: inherit;
+  }
+}
+button{
+  cursor: pointer;
+  /* border: none;
+  padding: 0; */
+}
+li {
+  list-style: none;
+}
+`;
+
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>
+);
---
+++ src/styled.d.ts
@@ -0,0 +1,13 @@
+// (참고: d.ts 는 declaration file 이라는 뜻이다.)
+// 타입스크립트 declaration(선언) file은 타입스크립트를 위한 일종의 설명이다.
+
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    textColor: string;
+    bgColor: string;
+    accentColor: string;
+    cardBgColor: string;
+  }
+}
